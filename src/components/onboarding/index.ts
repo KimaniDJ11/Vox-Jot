091691e09ca182ccab3200f414bcac540c0a4f6e
KimaniDJ11 <kimani@jamesdinamik.com>
2026-03-16 15:13:02 -0400

    feat: export OllamaSetup from onboarding index

--- src/components/onboarding/index.ts
+++ src/components/onboarding/index.ts
@@ -2,3 +2,4 @@ export { default } from "./Onboarding";
 export { default as AccessibilityOnboarding } from "./AccessibilityOnboarding";
 export { default as ModelCard } from "./ModelCard";
 export type { ModelCardStatus } from "./ModelCard";
+export { default as OllamaSetup } from "./OllamaSetup";
